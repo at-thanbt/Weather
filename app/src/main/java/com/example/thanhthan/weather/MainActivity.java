067f044396015f195e7dc1f0b56dabaae532960a
btthan <buithithan.cntt@gmail.com>
2017-01-08 21:10:17 +0700

    create tool bar

--- app/src/main/java/com/example/thanhthan/weather/MainActivity.java
+++ app/src/main/java/com/example/thanhthan/weather/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.thanhthan.weather;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.id_toolbar);
+        setSupportActionBar(myToolbar);
+
     }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu,menu);
+        return true;
+    }
+    public boolean onOptionItemsItemSelected(MenuItem item){
+        switch (item.getItemId()){
+            case R.id.city:
+                return true;
+            case R.id.add :
+                return true;
+            case R.id.more:
+                return  true;
+        }
+        return true;
+    }
+
+
 }
